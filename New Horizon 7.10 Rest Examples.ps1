3eb6fe75d86b8bc3d608ff7869df331b33a6f271
Andrew Morgan <andy@lab.local>
2019-09-18 12:47:05 +0100

    Added documentation and support for adding URLs.

--- New Horizon 7.10 Rest Examples.ps1
+++ New Horizon 7.10 Rest Examples.ps1
@@ -1,4 +1,6 @@
-﻿function Get-HRHeader(){
+﻿### Use this as a jump off point to start consuming the new horizon 7.10 rest api ###
+
+function Get-HRHeader(){
     param($accessToken)
     return @{
         'Authorization' = 'Bearer ' + $($accessToken.access_token)
@@ -12,7 +14,8 @@ function Open-HRConnection(){
     param(
         [string] $username,
         [string] $password,
-        [string] $domain
+        [string] $domain,
+        [string] $url
     )
 
     $Credentials = New-Object psobject -Property @{
@@ -20,90 +23,104 @@ function Open-HRConnection(){
         password = $password
         domain = $domain
     }
-    return invoke-restmethod -Method Post -uri "https://pod1hcon2.lab.local/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json)
+
+    return invoke-restmethod -Method Post -uri "$url/rest/login" -ContentType "application/json" -Body ($Credentials | ConvertTo-Json)
 }
 
 function Close-HRConnection(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method post -uri "https://pod1hcon2.lab.local/rest/logout" -ContentType "application/json" -Body ($accessToken | ConvertTo-Json)
+    return Invoke-RestMethod -Method post -uri "$url/rest/logout" -ContentType "application/json" -Body ($accessToken | ConvertTo-Json)
 }
 
 
 function Get-HRConnectionServers(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/connection-servers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/connection-servers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
 function Get-HREventDatabase(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/event-database" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/event-database" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
 function Get-HRADDomains(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/ad-domains" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/ad-domains" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
 function Get-HRViewComposers(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/view-composers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/view-composers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
 function Get-HRVirtualCenters(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/virtual-centers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/virtual-centers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
 function Get-HRFarms(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/farms" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/farms" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
 function Get-HRRDSServers(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/rds-servers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/rds-servers" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
 function Get-HRGateways(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/gateways" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/gateways" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
 function Get-HRSAMLAuthenticators(){
     param(
-        $accessToken
+        $accessToken,
+        $url
     )
-    return Invoke-RestMethod -Method Get -uri "https://pod1hcon2.lab.local/rest/monitor/saml-authenticators" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
+    return Invoke-RestMethod -Method Get -uri "$url/rest/monitor/saml-authenticators" -ContentType "application/json" -Headers (Get-HRHeader -accessToken $accessToken)
 }
 
-if($accessToken = Open-HRConnection -username (read-host -Prompt "username") -password (read-host -Prompt "password") -domain (read-host -Prompt "domain")){
-    Get-HRConnectionServers -accessToken $accessToken
-    Get-HRGateways -accessToken $accessToken
-    Get-HRSAMLAuthenticators -accessToken $accessToken
-    Get-HREventDatabase -accessToken $accessToken
-    Get-HRADDomains -accessToken $accessToken
-    Get-HRViewComposers -accessToken $accessToken
-    Get-HRVirtualCenters -accessToken $accessToken
-    Get-HRFarms -accessToken $accessToken
-    Get-HRRDSServers -accessToken $accessToken
-    Close-HRConnection -accessToken $accessToken
+
+$url = "https://pod1hcon2.lab.local"
+
+if($accessToken = Open-HRConnection -username (read-host -Prompt "username") -password (read-host -Prompt "password") -domain (read-host -Prompt "domain") -url $url){
+    Get-HRConnectionServers -accessToken $accessToken -url $url
+    Get-HRGateways -accessToken $accessToken -url $url
+    Get-HRSAMLAuthenticators -accessToken $accessToken -url $url
+    Get-HREventDatabase -accessToken $accessToken -url $url
+    Get-HRADDomains -accessToken $accessToken -url $url
+    Get-HRViewComposers -accessToken $accessToken -url $url
+    Get-HRVirtualCenters -accessToken $accessToken -url $url
+    Get-HRFarms -accessToken $accessToken -url $url
+    Get-HRRDSServers -accessToken $accessToken -url $url
+    Close-HRConnection -accessToken $accessToken -url $url
 }
 
