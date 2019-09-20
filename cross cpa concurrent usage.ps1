645a90dac1dbca9a9df48ea36d84d5768e63e66c
Andrew Morgan <andy@lab.local>
2019-09-20 10:22:59 +0100

    updated the cross cpa concurrent usage script

--- cross cpa concurrent usage.ps1
+++ cross cpa concurrent usage.ps1
@@ -2,20 +2,18 @@
 
 import-module vmware.powercli
 
-function Get-GlobalSessionQuery{
+function Get-LocalSessionQuery{
     param(
         [parameter(mandatory=$true)]
-        $hvServer,
-        [parameter(mandatory=$true)]
-        $pod
+        $hvServer
     )
     $Results=@()
     
-    $queryspec = new-object VMware.Hv.GlobalSessionQueryServiceQuerySpec
-    $queryspec.Pod = $pod
-    $queryspec.MaxPageSize= 1
-
-    $queryResponse = $hvserver.ExtensionData.GlobalSessionQueryService.GlobalSessionQueryService_QueryWithSpec($queryspec)
+   $queryService = New-Object VMware.Hv.QueryServiceService
+   $defn = New-Object VMware.Hv.QueryDefinition
+   $defn.queryEntityType = 'SessionLocalSummaryView'
+   
+    $queryResponse = $queryService.QueryService_Create($hvserver.ExtensionData,$defn)
     $results+=$queryResponse.Results
     Write-Verbose "Received $($queryresponse.results.count)"
     if($queryResponse.RemainingCount -gt 0){
@@ -23,7 +21,7 @@ function Get-GlobalSessionQuery{
         $remaining=$queryResponse.RemainingCount
         do{
             
-            $latestResponse = $hvserver.ExtensionData.GlobalSessionQueryService.GlobalSessionQueryService_GetNext($queryResponse.Id)
+            $latestResponse = $queryService.QueryService_GetNext($hvserver.ExtensionData,$queryResponse.Id)
             $results+= $latestResponse.Results
             Write-Verbose "Pulled an additional $($latestResponse.Results.Count) item(s)"
             $remaining = $latestResponse.RemainingCount
@@ -35,44 +33,124 @@ function Get-GlobalSessionQuery{
     $results
 }
 
-$serveraddress = "pod1hcon1.lab.local"
-$creds = Get-Credential -Message "Please enter valid credentials to connect to the Horizon Connection servers"
-$hvserver = Connect-HVServer $serveraddress -Credential $creds
-$pods = $hvserver.ExtensionData.pod.Pod_List()
-$sessionData=@()
-$time = (get-date)
-$sessionList=@()
-foreach($pod in $pods){
-    
-    $sessionData+=Get-GlobalSessionQuery $hvserver $pod.id -Verbose
+
+function convert-SessionsToReportObject(){
+    param(
+        [psobject]$sessionlist,
+        [string]$siteName,
+        [string]$podName
+    )
+    $List =@()
+    Write-Verbose $($sessionList.Count)
+    foreach($session in $sessionList){
+        $hostingResource=""
+        if($session.NamesData.DesktopName.Length -gt 0){
+            $hostingResource = $session.NamesData.DesktopName
+        }
+        elseif($session.NamesData.FarmName.length -gt 0){
+            $hostingResource = $session.NamesData.FarmName
+        }
+        $SessionObject = New-Object PSObject -Property @{
+            username         = $session.NamesData.UserName
+            domain           = $session.Namesdata.username.split('\')[0]
+            machineName      = $session.NamesData.MachineOrRDSServerName
+            clientName       = $session.NamesData.ClientName
+            SessionType      = $session.SessionData.SessionType
+            SessionState     = $session.SessionData.SessionState
+            StartTime        = $session.SessionData.StartTime
+            DisconnectTime   = $session.SessionData.DisconnectTime
+            HostingResource  = $hostingResource
+            PodName          = $podName
+            SiteName         = $siteName
+        }
+    $List+=$SessionObject
+    }
+    return $List
+
 }
 
+$time = (get-date)
+$sessionList=@()
 
-Disconnect-HVServer -Confirm:$false
+$serveraddress = Read-Host -Prompt "enter a connection server name to connect to"
+$creds = Get-Credential -Message "Please enter valid credentials to connect to the Horizon Connection servers"
+$hvserver = Connect-HVServer $serveraddress -Credential $creds
 
+#List Pods and Sites for reports
+$pods = $hvserver.ExtensionData.pod.Pod_List()
+$sites = $hvserver.ExtensionData.Site.Site_List()
+
+
+#get Local Pod
+$localPod = $pods | ?{$_.localpod}[0]
+#Get Local Site
+$localSite = $sites | ?{$_.id.id -eq $localPod.site.Id}[0]
+
+
+#Gather Data from local pod
+$localsessions = Get-LocalSessionQuery($hvserver)
+Write-Verbose "Received $($localsessions.count) sessions from local pod"
+$sessionList += convert-SessionsToReportObject -SessionList $localsessions -podName $localPod.DisplayName -siteName $localSite.base.displayname
+
+#Perform task on Remote Pods
+
+foreach($pod in $pods | ? {!$_.localPod}){
+    
+    $localSite = $sites | ?{$_.id.id -eq $pod.site.Id}[0]
+    $hasData=$false
+    foreach($endpoint in $pod.endpoints){        
+        $endpointDetails = $hvserver.ExtensionData.PodEndpoint.PodEndpoint_Get($endpoint);
+        $connectionURI = [System.Uri] $endpointDetails.serverAddress
+        $servername = $($connectionURI.DnsSafeHost)
+        try{
+            Write-Verbose "Connecting to $servername for Pod: $($pod.displayname)"
+            $remoteHVserver = Connect-HVServer -Server $servername -Credential $creds
+            $remoteSessions = Get-LocalSessionQuery -hvServer $remoteHVserver
+            Write-Verbose "Received $($remoteSessions.count) for Pod: $($Pod.displayname)"
+            $sessionList += convert-SessionsToReportObject -SessionList $remoteSessions -podName $pod.DisplayName -siteName $localSite.base.displayname
+            $hasData=$true
+            Disconnect-HVServer -Server $servername -Force -Confirm:$false
+            break
+        }
+        catch{
+            Disconnect-HVServer -Server $servername -Force -Confirm:$false -ea SilentlyContinue
+            write-warning "An error Occurred while connecting to the connection server: $connectionURL in Pod: $($pod.displayname)"
+        }
+    }
+    if(!$hasData){
+        write-error "Could not retrieve any data from remote pod: $($pod.displayname)"
+    }
+}
+
+#close initial connection
+Disconnect-HVServer -Server $serveraddress -Force -Confirm:$false -ea SilentlyContinue
+
+Write-Verbose "Received a total of $($sessionList.count) sessions across the CPA"
+
+#parse data
 foreach($session in $sessiondata){
     $hostingResource=""
     if($session.NamesData.BaseNames.DesktopName.Length -gt 0){
-        $hostingResource = $session.NamesData.BaseNames.DesktopName
+        $hostingResource = $session.NamesData.DesktopName
     }
     elseif($session.NamesData.BaseNames.FarmName.length -gt 0){
-        $hostingResource = $session.NamesData.BaseNames.FarmName
+        $hostingResource = $session.NamesData.FarmName
     }
     $SessionObject = New-Object PSObject -Property @{
-        username         = $session.NamesData.BaseNames.UserName
-        domain           = $session.Namesdata.baseNames.username.split('\')[0]
-        machineName      = $session.NamesData.BaseNames.MachineOrRDSServerName
-        clientName       = $session.NamesData.BaseNames.ClientName
+        username         = $session.NamesData.UserName
+        domain           = $session.Namesdata.username.split('\')[0]
+        machineName      = $session.NamesData.MachineOrRDSServerName
+        clientName       = $session.NamesData.ClientName
         SessionType      = $session.SessionData.SessionType
         SessionState     = $session.SessionData.SessionState
-        SiteName         = $session.NamesData.SiteName
-        PodName          = $session.namesData.PodName
         StartTime        = $session.SessionData.StartTime
         DisconnectTime   = $session.SessionData.DisconnectTime
         HostingResource  = $hostingResource
     }
     $sessionList+=$SessionObject
 }
+
+$podReport = $sessionlist | group-object -
 
 $podReport=@()
     $sessionList | group-object podname | %{
@@ -92,18 +170,26 @@ $siteReport=@()
         $sitereport+=$site
 }
 
+$domainReport=@()
+    $sessionList | group-object domain | %{
+        $domain = new-object psobject -Property @{
+            domainname = $_.name
+            Count = $_.count
+        }
+        $domainReport+=$domain
 
-
+}
 
 
 $report = New-Object PSObject -Property @{
         timeStamp                 = $time
-        totalUniqueCCU            = ($Sessionlist | group-object username).Count
-        totalUniqueCCUApplication = ($Sessionlist | ?{$_.sessionType -eq "APPLICATION"} | group-object username).Count
-        totalUniqueCCUbyClient    = ($Sessionlist | group-object username, clientname).Count
+        totalUniqueCCU            = @($Sessionlist | group-object username).Count
+        totalUniqueCCUApplication = @($Sessionlist | ?{$_.sessionType -eq "APPLICATION"} | group-object username).Count
+        totalUniqueCCUbyClient    = @($Sessionlist | group-object username, clientname).Count
         totalSessions             = $sessionList.Count
         PodCounts                 = $podReport
-        siteCounts                 = $siteReport
+        siteCounts                = $siteReport
+        domainCounts              = $domainReport
+}
+$report
 
-    }
-$sessionlist
